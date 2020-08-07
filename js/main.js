31dca6e202ff920b4670059d9b3d76aa66c33c42
Kailah Konkel <kailahkonkel@Kailahs-MBP.home>
2020-08-07 13:50:56 -0400

    with card styling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
+deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+
+ //declare variables
+ 
+ let deck1, deck2 = []
+
+ //cached element references
+ const deck1El = document.getElementById('deck1')
+ const deck2El = document.getElementById('deck2')
+ const flipBtn = document.getElementById('btn')
+ 
+ flipBtn.addEventListener('click', handleClick)
+ 
+ init()
+ 
+ function init(){
+     deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+ }
+ 
 /*--------Variabels-------*/
 // Create the variables for the players.
 let player = ['1', '2']
 let total;
+let deck1, deck2,
 
 /*-------Cached Element References--------*/
 // Create the cached element references for the buttons.
@@ -10,7 +30,7 @@ const hitBtn = document.getElementById('hitBtn')
 const standBtn = document.getElementById('standBtn')
 
 /*-------Event Listeners-------*/
-startBtn.addEventListener('click', init;
+startBtn.addEventListener('click', init)
 hitBtn.addEventListener('click', hit)
 standBtn.addEventListener('click', stand)
 
@@ -46,3 +66,5 @@ function stand(){
 // Create the checkWinner function, winner with the closest to 21 without going over wins
     //include here the check if the player busted(going over 21)
 // Include messages specified to whether the game is won, lost, or tied(had the same number). At the end of this add, Congrats or too bad or it's a tie, then after add, click Start a Game to play again!
+ 
+ 
