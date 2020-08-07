94940e3b184cc3e322f856d9a5814e54c2ef347b
Kailah Konkel <kailahkonkel@Kailahs-MBP.home>
2020-08-07 11:43:29 -0400

    add card deck & style

---
+++ js/main.js
@@ -0,0 +1,48 @@
+/*--------Variabels-------*/
+// Create the variables for the players.
+let player = ['1', '2']
+let total;
+
+/*-------Cached Element References--------*/
+// Create the cached element references for the buttons.
+const startBtn = document.getElementById('startBtn')
+const hitBtn = document.getElementById('hitBtn')
+const standBtn = document.getElementById('standBtn')
+
+/*-------Event Listeners-------*/
+startBtn.addEventListener('click', init;
+hitBtn.addEventListener('click', hit)
+standBtn.addEventListener('click', stand)
+
+/*-------Functions-------*/
+// Create the initial function and call for it code the card deck.
+init();
+
+function init(){
+    let total = 0;
+    let player = '1';
+
+}
+
+function hit(){
+
+}
+
+function stand(){
+
+}
+
+// Create a function for shuffling the cards.
+
+
+// Create the start game function.
+
+// Get the total to show up once the first cards are dealt
+// Create the function to deal the hand.
+// Create the render function for the cards.
+// Create the hit function.
+// Get the total of the cards to correspond with the hit function.
+// Create the stay function.
+// Create the checkWinner function, winner with the closest to 21 without going over wins
+    //include here the check if the player busted(going over 21)
+// Include messages specified to whether the game is won, lost, or tied(had the same number). At the end of this add, Congrats or too bad or it's a tie, then after add, click Start a Game to play again!
